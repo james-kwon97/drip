76fd5dd489a5c351f07e74137cc884c47e3172c1
James Kwon <jhk1097@gmail.com>
2024-08-27 23:55:30 +1200

    Icons added to left section of navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
+import {
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline'
 import './Navbar.css'
 
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="navbar-left">
+        <a href="#search" className="navbar-icon">
+          <MagnifyingGlassIcon className="icon" />
+        </a>
+        <a href="#shopping" className="navbar-icon">
+          <ShoppingBagIcon className="icon" />
+        </a>
+        <a href="#user" className="navbar-icon">
+          <UserIcon className="icon" />
+        </a>
+        {/* Switch and Maori text */}
+      </div>
       <div className="navbar-logo">
         <h1>drip:</h1>
       </div>
-      <div className="navbar-links">
-        <a href="#about-us">About Us</a>
-        <a href="#shop">Shop</a>
-        <a href="#contact">Contact</a>
+      <div className="navbar-right">
+        <a href="#about-us" className="navbar-button">
+          About Us
+        </a>
+        <a href="#shop" className="navbar-button">
+          Shop
+        </a>
+        <a href="#locations" className="navbar-button">
+          Locations
+        </a>
       </div>
     </nav>
   )
