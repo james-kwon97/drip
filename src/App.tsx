201db9946273f8e097cd4202f80bdd4ed5dbb343
James Kwon <jhk1097@gmail.com>
2024-08-29 14:40:41 +1200

    Install boostrap icon dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div className="app-container">
       <Navbar />
+      <Home />
       {/* Add other components or content below */}
     </div>
   )
