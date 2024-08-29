f2e124b1debdb3a9cbff83f7343ae3439d4a41e5
James Kwon <jhk1097@gmail.com>
2024-08-29 15:40:26 +1200

    Set fixed background image only for the loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 
 function App() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY
+      setIsScrolled(scrollPosition > window.innerHeight)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div className="app-container">
+    <div className="app">
       <Navbar />
-      <Home />
-      {/* Add other components or content below */}
+      <div className="hero-section">
+        <Home />
+      </div>
+      <div className="content-section">{/* Add other components here */}</div>
     </div>
   )
 }
