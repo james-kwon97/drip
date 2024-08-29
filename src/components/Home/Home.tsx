358a7855e42ce64dd7c4b22ee53f4456e2313fe7
James Kwon <jhk1097@gmail.com>
2024-08-29 23:29:33 +1200

    Back to old branch, due to some errors

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,15 +2,14 @@ import React from 'react'
 import './Home.css'
 import arrowIcon from '../../assets/arrow_icon.png'
 import aboutImage from '../../assets/about_image.png'
-import productImage from '../../assets/product_image.png'
 
 function Home() {
   return (
     <div className="home">
       <div className="main-content">
         <div className="about-section">
-          <h2 className="about-title">About</h2>
-          <p className="about-description">
+          <h2 className="about">About</h2>
+          <p className="about-info">
             You’ll be greeted by stunning visuals of our freshly brewed
             beverages, delicious pastries, and cozy atmosphere.
           </p>
@@ -21,55 +20,17 @@ function Home() {
             <img src={arrowIcon} alt="arrow icon" className="arrow-icon" />
           </div>
         </div>
-
         <img src={aboutImage} alt="Coffee in a cup" className="about-image" />
 
         <div className="product-section">
           <h2 className="product-title">Our Products</h2>
           <div className="product-link-container">
             <a href="#products" className="product-link">
-              Shop Now
+              Shop now
             </a>
             <img src={arrowIcon} alt="arrow icon" className="arrow-icon" />
           </div>
-
-          <div className="products-list">
-            <div className="product-item">
-              <img
-                src={productImage}
-                alt="Drip product 1"
-                className="product-image"
-              />
-              <div className="product-details">
-                <span className="product-name">Drip Blend</span>
-                <span className="product-price">From $10.00</span>
-              </div>
-            </div>
-            <div className="product-item">
-              <img
-                src={productImage}
-                alt="Drip product 2"
-                className="product-image"
-              />
-              <div className="product-details">
-                <span className="product-name">Drip Blend</span>
-                <span className="product-price">From $15.00</span>
-              </div>
-            </div>
-            <div className="product-item">
-              <img
-                src={productImage}
-                alt="Drip product 3"
-                className="product-image"
-              />
-              <div className="product-details">
-                <span className="product-name">Drip Blend</span>
-                <span className="product-price">From $20.00</span>
-              </div>
-            </div>
-          </div>
         </div>
-
         <div className="location-section">
           <h2>Locations</h2>
           <p>Information about our locations will be provided here.</p>
