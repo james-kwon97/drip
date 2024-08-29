21c332ddd31a0259463337aa541cd81f3b0419ae
James Kwon <jhk1097@gmail.com>
2024-08-29 14:52:21 +1200

    Styled and positioned icon and scroll down text at the bottom of the page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Home.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function Home() {
   return (
     <div className="home">
-      <h2>Home Content</h2>
+      <div className="icon-container">
+        <i
+          className="bi bi-mouse"
+          style={{ fontSize: '42px', color: 'white' }}
+        ></i>
+        <p>scroll down</p>
+      </div>
     </div>
   )
 }
