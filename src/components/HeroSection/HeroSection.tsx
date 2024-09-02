b228f4af2d263618c9e92d0d9b0a03d88ac60a25
James Kwon <jhk1097@gmail.com>
2024-09-03 10:37:29 +1200

    Add useEffect to show or hide herosection icon and text upon scrolling

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './HeroSection.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function HeroSection() {
+  const [isVisible, setIsVisible] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY
+      if (scrollPosition >= 145) {
+        setIsVisible(false)
+      } else {
+        setIsVisible(true)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <div className="hero-content">
-      <div className="icon-container">
+      <div className={`icon-container ${isVisible ? '' : 'hidden'}`}>
         <i
           className="bi bi-mouse"
           style={{ fontSize: '36px', color: 'white' }}
